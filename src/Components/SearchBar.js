3342337b740774cc307c3a672898c7d257af9f47
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-17 17:01:32 -0600

    search bar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,3 +1,10 @@
-export default function searchNotes() {
-  return <div></div>;
+import SearchBar from "./SearchBar";
+
+export default function Header({ onSearch }) {
+  return (
+    <header>
+      <h1 className="title"> Sticky Notes</h1>
+      <SearchBar onSearch={onSearch} />
+    </header>
+  );
 }
