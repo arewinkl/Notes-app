2ad9cf33f82c83e02fae2b449ed622705d16f3c6
Alec Rewinkel <alecrewinkel@gmail.com>
2022-06-18 09:41:42 -0600

    css chalkboard

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import SearchBar from "./SearchBar";
 export default function Header({ onSearch }) {
   return (
     <header>
-      <h1 className="title"> Sticky Notes</h1>
+      <h1 className="title"> Sticky Note Manager</h1>
       <SearchBar onSearch={onSearch} />
     </header>
   );
